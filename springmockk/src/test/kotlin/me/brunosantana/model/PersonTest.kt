13c0f9912006cf6d87313d7925fc0df44b0f792c
Bruno Sant' Ana <bruno.santana.ti@gmail.com>
2022-09-26 17:28:47 -0300

    Mockar o retorno de uma extension function de um map usando mockkStatic e unmockkStatic

--- springmockk/src/test/kotlin/me/brunosantana/model/PersonTest.kt
+++ springmockk/src/test/kotlin/me/brunosantana/model/PersonTest.kt
@@ -1,5 +1,10 @@
 package me.brunosantana.model
 
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.mockkStatic
+import io.mockk.unmockkStatic
+import me.brunosantana.utils.getString
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 import java.math.BigDecimal
@@ -18,6 +23,20 @@ class PersonTest {
         Assertions.assertNotNull(person)
         Assertions.assertEquals("Bruno", person?.name)
         Assertions.assertEquals(BigDecimal("1000"), person?.salary)
+    }
+
+    @Test
+    fun `should return null if map to Person conversion fails`() {
+        mockkStatic("me.brunosantana.utils.ExtensionsKt")
+
+        val mapMock = mockk<MutableMap<String, MyValue>>()
+        every { mapMock.getString(any()) } throws RuntimeException("Convertion error")
+
+        val person = Person.mapToPerson(mapMock)
+
+        Assertions.assertNull(person)
+
+        unmockkStatic("me.brunosantana.utils.ExtensionsKt")
     }
 
 }
