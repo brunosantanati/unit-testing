0727f1caf83afd684efe9143907eab142c7efef4
Bruno Sant' Ana <bruno.santana.ti@gmail.com>
2022-09-26 16:48:13 -0300

    Projeto vazio para usar springmockk

---
+++ springmockk/src/main/kotlin/me/brunosantana/SpringmockkApplication.kt
@@ -0,0 +1,11 @@
+package me.brunosantana
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringmockkApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringmockkApplication>(*args)
+}
