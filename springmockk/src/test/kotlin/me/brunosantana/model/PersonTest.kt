d46a8d0468b4419210a7212ba1170933518aef9d
Bruno Sant' Ana <bruno.santana.ti@gmail.com>
2022-09-27 10:55:12 -0300

    Compare all attributes of 2 objects recursively

--- springmockk/src/test/kotlin/me/brunosantana/model/PersonTest.kt
+++ springmockk/src/test/kotlin/me/brunosantana/model/PersonTest.kt
@@ -6,37 +6,98 @@ import io.mockk.mockkStatic
 import io.mockk.unmockkStatic
 import me.brunosantana.utils.getString
 import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import java.math.BigDecimal
+import org.assertj.core.api.Assertions as AssertionsAssertj
 
 class PersonTest {
 
-    @Test
-    fun `should convert map to Person successfully`() {
-        val map = mutableMapOf(
-            "name" to MyValue("Bruno", null),
-            "salary" to MyValue(null, BigDecimal("1000"))
-        )
+    @Nested
+    @DisplayName("Test map to Person")
+    inner class MapToPersonTests {
+        @Test
+        fun `should convert map to Person successfully`() {
+            val map = mutableMapOf(
+                "name" to MyValue("Bruno", null),
+                "salary" to MyValue(null, BigDecimal("1000"))
+            )
 
-        val person = Person.mapToPerson(map)
+            val person = Person.mapToPerson(map)
 
-        Assertions.assertNotNull(person)
-        Assertions.assertEquals("Bruno", person?.name)
-        Assertions.assertEquals(BigDecimal("1000"), person?.salary)
+            Assertions.assertNotNull(person)
+            Assertions.assertEquals("Bruno", person?.name)
+            Assertions.assertEquals(BigDecimal("1000"), person?.salary)
+        }
+
+        @Test
+        fun `should return null if map to Person conversion fails`() {
+            mockkStatic("me.brunosantana.utils.ExtensionsKt")
+
+            val mapMock = mockk<MutableMap<String, MyValue>>()
+            every { mapMock.getString(any()) } throws RuntimeException("Convertion error")
+
+            val person = Person.mapToPerson(mapMock)
+
+            Assertions.assertNull(person)
+
+            unmockkStatic("me.brunosantana.utils.ExtensionsKt")
+        }
     }
 
-    @Test
-    fun `should return null if map to Person conversion fails`() {
-        mockkStatic("me.brunosantana.utils.ExtensionsKt")
+    @Nested
+    @DisplayName("Comparing")
+    inner class Compare2People {
 
-        val mapMock = mockk<MutableMap<String, MyValue>>()
-        every { mapMock.getString(any()) } throws RuntimeException("Convertion error")
+        @Test
+        fun `should compare all fields of 2 people that are equal`() {
+            val person1 = Person(name = "Bruno", salary = BigDecimal("1000"))
+            val person2 = Person(name = "Bruno", salary = BigDecimal("1000"))
 
-        val person = Person.mapToPerson(mapMock)
+            AssertionsAssertj.assertThat(person1)
+                .usingRecursiveComparison()
+                .isEqualTo(person2)
+        }
 
-        Assertions.assertNull(person)
+        @Test
+        fun `should compare all fields of 2 people that are NOT equal`() {
+            val person1 = Person(name = "Bruno", salary = BigDecimal("1000"))
+            val person2 = Person(name = "Bruno", salary = BigDecimal("2000"))
 
-        unmockkStatic("me.brunosantana.utils.ExtensionsKt")
+            AssertionsAssertj.assertThat(person1)
+                .usingRecursiveComparison()
+                .isNotEqualTo(person2)
+
+            val person3 = Person(name = "Bruno", salary = BigDecimal("1000"))
+            val person4 = Person(name = "Jackson", salary = BigDecimal("1000"))
+
+            AssertionsAssertj.assertThat(person3)
+                .usingRecursiveComparison()
+                .isNotEqualTo(person4)
+        }
+
+        @Test
+        fun `should compare all fields of 2 people ignoring salary`() {
+            val person1 = Person(name = "Bruno", salary = BigDecimal("1000"))
+            val person2 = Person(name = "Bruno", salary = BigDecimal("2000"))
+
+            AssertionsAssertj.assertThat(person1)
+                .usingRecursiveComparison()
+                .ignoringFields("salary")
+                .isEqualTo(person2)
+        }
+
+        @Test
+        fun `should compare all fields of 2 people ignoring name`() {
+            val person1 = Person(name = "Bruno", salary = BigDecimal("1000"))
+            val person2 = Person(name = "Jackson", salary = BigDecimal("1000"))
+
+            AssertionsAssertj.assertThat(person1)
+                .usingRecursiveComparison()
+                .ignoringFields("name")
+                .isEqualTo(person2)
+        }
+
     }
-
 }
